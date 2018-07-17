e77bf79c34c65bcf6b9a66a14de2533c0aae6f8b
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-17 18:02:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,15 @@
 class Dog
-  def initialize(name, breed)
-    @name =(name)
+ 
+  def initialize(name)
+    @name = name
   end
-  
-  def name=(new_name)
-    @name= new_name
-  end
-
+ 
   def name
     @name
-  end  
-  
-  def breed=(new_breed)
-    @breed=new_breed
   end
-
-  def breed
-    @breed
-  end    
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
 end
