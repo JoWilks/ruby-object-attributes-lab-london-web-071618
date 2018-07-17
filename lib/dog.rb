a3ea586b327996a04bd0ffe7b1f5b847849ba955
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-17 17:56:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,16 @@ class Dog
     @breed = breed
   end
   
-  def name=get_name
-    @name= get_name
+  def name=(get_name)
+    @name=(get_name)
   end
 
   def name
     @name
   end  
   
-  def breed=get_breed
-    @breed= get_breed
+  def breed=(get_breed)
+    @breed=(get_breed)
   end
 
   def breed
